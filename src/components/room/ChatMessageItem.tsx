8b3c5d12227278bf1914fcedd74f0abebaec58b8
Mevlüt Can Tuna <mttuna90@gmail.com>
2021-07-15 14:45:53 +0300

    some bugs fixed

--- src/components/room/ChatMessageItem.tsx
+++ src/components/room/ChatMessageItem.tsx
@@ -12,41 +12,6 @@ const ChatMessageItem: React.FC<IProps> = (props) => {
 
   const mainDivRef = React.useRef<HTMLDivElement | null>(null);
 
-  const timeSince = (timeStamp: number) => {
-    let now = new Date(),
-      secondsPast = (now.getTime() - timeStamp) / 1000;
-    if (secondsPast < 60) {
-      // @ts-ignore
-      return parseInt(secondsPast) + "s";
-    }
-    if (secondsPast < 3600) {
-      // @ts-ignore
-      return parseInt(secondsPast / 60) + "m";
-    }
-    if (secondsPast <= 86400) {
-      // @ts-ignore
-      return parseInt(secondsPast / 3600) + "h";
-    }
-    if (secondsPast > 86400) {
-      // @ts-ignore
-      let day = timeStamp.getDate();
-      // @ts-ignore
-      let month = timeStamp
-        // @ts-ignore
-        .toDateString()
-        .match(/ [a-zA-Z]*/)[0]
-        .replace(" ", "");
-      // @ts-ignore
-      let year =
-        // @ts-ignore
-        timeStamp.getFullYear() === now.getFullYear()
-          ? ""
-          : // @ts-ignore
-            " " + timeStamp.getFullYear();
-      return day + " " + month + year;
-    }
-  };
-
   useEffect(() => {
     //auto scroll to bottom of messages
     if (mainDivRef.current !== undefined) {
@@ -54,14 +19,13 @@ const ChatMessageItem: React.FC<IProps> = (props) => {
     }
   }, []);
 
+  console.log(item.value);
+
   return (
     <div className="chatMessageItem" ref={mainDivRef}>
       <div className="chatMessageItem__left">
         <img alt={"avatar"} src={item.value.user.avatar} />
         <span>{item.value.content}</span>
-      </div>
-      <div className="chatMessageItem__right">
-        {timeSince(item.value.timestamp) + " ago"}
       </div>
     </div>
   );
